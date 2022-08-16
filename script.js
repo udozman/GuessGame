19ed8f3d26255255443cd17b6e56374a14e601f1
udozman <103385204+udozman@users.noreply.github.com>
2022-08-16 08:38:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+'use strict';
+let secretNumber = Math.floor((Math.random() * 20) + 1);
+console.log(secretNumber);
+
+let score = 20;
+let highScore = 20;
+
+document.querySelector(".check").addEventListener('click', function(){
+  const guess = Number((document.querySelector('.guess').value));
+
+  if(!guess){
+    document.querySelector('.message').textContent = "Pick a Number!";
+  }else if(guess === secretNumber){
+    if(score > 1){
+      document.querySelector('.message').textContent = "Correct Number😊";
+        document.querySelector('body').style.backgroundColor = '#64eb34';
+        document.querySelector('.number').textContent = secretNumber;
+        if(score > highScore){
+          highScore = score;
+        }
+        document.querySelector('.highscore').textContent = highScore;
+    }else{
+      document.querySelector('.message').textContent = "Correct but you've run out of guesses😒";
+      document.querySelector('body').style.backgroundColor = '#34b7eb';
+      document.querySelector('.number').textContent = secretNumber;
+      score = 20;
+    }
+
+  }else if(guess > secretNumber){
+    if(score > 1){
+      document.querySelector('.message').textContent = "Too High!";
+      score--;
+      highScore--;
+      document.querySelector('.score').textContent = score;
+    }else{
+      document.querySelector('.message').textContent = "You Lose 😒!";
+      document.querySelector('.score').textContent = 0;
+    }
+
+  }else if(guess< secretNumber){
+    if(score > 1){
+      document.querySelector('.message').textContent = "Too Low!";
+      score--;
+      highScore--;
+      document.querySelector('.score').textContent = score;
+    }else{
+      document.querySelector('.message').textContent = "You Lose 😒!";
+      document.querySelector('.score').textContent = 0;
+    }
+
+  }
+})
+//WHEN AGAIN! BUTTON IS PRESSED
+document.querySelector('.again').addEventListener('click', function(){
+  document.querySelector('body').style.backgroundColor = "#222";
+  document.querySelector('.message').textContent = "Start guessing...";
+  highScore = score;
+  document.querySelector('.score').textContent = "20";
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  secretNumber = Math.floor((Math.random() * 20) + 1);
+  console.log(secretNumber);
+})
